4fb5aceeea4ad41093792238b32fcd2ec1b0b535
Maksl5 <markusbensing@googlemail.com>
2012-09-11 01:40:24 +0200

    Added SSL Support for the NetworkThread.

--- src/com/maksl5/bl_hunt/NetworkThread.java
+++ src/com/maksl5/bl_hunt/NetworkThread.java
@@ -16,13 +16,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerPNames;
+import org.apache.http.conn.params.ConnPerRouteBean;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.util.EntityUtils;
 
 import com.maksl5.bl_hunt.R.id;
@@ -64,6 +75,9 @@ public class NetworkThread extends AsyncTask<String, Integer, String> {
 
 		String remoteFile = params[0];
 		int requestId = Integer.parseInt(params[1]);
+		boolean https = false;
+
+		if (remoteFile.startsWith("https")) https = true;
 
 		try {
 
@@ -80,7 +94,31 @@ public class NetworkThread extends AsyncTask<String, Integer, String> {
 
 			URI httpUri = URI.create(remoteFile);
 
-			HttpClient httpClient = new DefaultHttpClient();
+			// SSL Implementation
+
+			HttpClient httpClient;
+			
+			if (https) {
+
+				SchemeRegistry schemeRegistry = new SchemeRegistry();
+				// http scheme
+				schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+				// https scheme
+				schemeRegistry.register(new Scheme("https", new EasySSLSocketFactory(), 443));
+
+				HttpParams httpParams = new BasicHttpParams();
+				httpParams.setParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 30);
+				httpParams.setParameter(ConnManagerPNames.MAX_CONNECTIONS_PER_ROUTE, new ConnPerRouteBean(30));
+				httpParams.setParameter(HttpProtocolParams.USE_EXPECT_CONTINUE, false);
+				HttpProtocolParams.setVersion(httpParams, HttpVersion.HTTP_1_1);
+
+				ClientConnectionManager cm = new ThreadSafeClientConnManager(httpParams, schemeRegistry);
+
+				httpClient = new DefaultHttpClient(cm, httpParams);
+			}
+			else {
+				httpClient = new DefaultHttpClient();
+			}
 			HttpPost postRequest = new HttpPost(httpUri);
 
 			postRequest.setEntity(new UrlEncodedFormEntity(postValues));
@@ -94,17 +132,17 @@ public class NetworkThread extends AsyncTask<String, Integer, String> {
 		catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return "<requestID='" + requestId + "' />" + "NA(-1)(1)";
+			return "<requestID='" + requestId + "' />" + "NA(-1)(1)\n" + e.getMessage();
 		}
 		catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return "<requestID='" + requestId + "' />" + "NA(-1)(2)";
+			return "<requestID='" + requestId + "' />" + "NA(-1)(2)\n" + e.getMessage();
 		}
 		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return "<requestID='" + requestId + "' />" + "NA(-1)(3)";
+			return "<requestID='" + requestId + "' />" + "NA(-1)(3)\n" + e.getMessage();
 		}
 
 		// TODO Auto-generated method stub
@@ -127,7 +165,7 @@ public class NetworkThread extends AsyncTask<String, Integer, String> {
 		result = reqIdMatcher.replaceFirst("");
 
 		mainActivity.authentification.fireOnNetworkResultAvailable(reqId, result);
-		
+
 		networkMananger.threadFinished(this);
 
 	}
